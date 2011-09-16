5fe558adf83fc94e332e344741a0fbe11e1576de
Muneeb <Muneeb@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-09-16 03:24:50 +0000

    git-svn-id: https://plugins.svn.wordpress.org/site-is-offline-plugin/trunk@438875 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- site-offline-options.php
+++ site-offline-options.php
@@ -16,6 +16,13 @@
                             <h3 class="hndle"><span><?php _e('Content/HTML Code'); ?></span></h3>
 
                             <div class="inside">
+<form action="https://www.paypal.com/cgi-bin/webscr" method="post">
+<input type="hidden" name="cmd" value="_s-xclick">
+<input type="hidden" name="hosted_button_id" value="3NW3EZG98RYYQ">
+<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+</form>
+
 								<p>Enabled: <label><input type="radio" value="true" name="cp_siteoffline_enabled" <?php echo ($options['enabled'] === true ? 'checked' : '' ); ?> />Yes</label> <label><input type="radio" value="false" name="cp_siteoffline_enabled" <?php echo ($options['enabled'] === false ? 'checked' : '' ); ?> /> No</label></p>
                                	<p><textarea spellcheck='false' rows='22' name="cp_siteoffline_content"><?php  
                                		if ( $options['content'] === NULL )
