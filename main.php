049f0386b67f7a769aa9d42483436cf43ff6525e
Muneeb <Muneeb@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-05-19 17:05:34 +0000

    git-svn-id: https://plugins.svn.wordpress.org/site-is-offline-plugin/tags/1.2@546242 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- main.php
+++ main.php
@@ -1,13 +1,13 @@
 <?php
 /*
 Plugin Name: Site Offline or Coming Soon
-Plugin URI:http://plugistan.com/comingsoon-plus/
+Plugin URI: http://imuneeb.com/wordpress-coming-soon-plugin/
 Description: Make safe changes to your site by enabling site offline mode with this plugin you'll be able to navigate your site normally but your regular visitor will se a site offline or coming soon page
 Version: 1.3
 Author: Muneeb
-Author URI: http://plugistan.com/comingsoon-plus/
+Author URI: http://imuneeb.com
 License: GPLv2 or later
-CopyRight (c) 2011 Muneeb ur Rehman
+CopyRight (c) 2011 Muneeb ur Rehman http://imuneeb.com
 */
 
 function cp_siteoffline_activate()
